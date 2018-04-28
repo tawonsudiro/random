4193542e071bd636c2d2497a09932ce37853c19e
tawonsudiro <tawonsudiro@gmail.com>
2018-04-28 15:29:23 +0700

    Update summon.js

--- summon.js
+++ summon.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="/2018/01/keeping-your-heart-and-brain-healthy.html"}
 if(x=="2"){xxx="/2018/01/detox-body-and-live-longer.html"}
 if(x=="3"){xxx="/2018/01/do-you-know-why-heartbeats-make-noise.html"}
-if(x=="4"){xxx="/2018/01/foods-for-your-teeth.html"}a_to_ve[i].href="http://sikomos.tech"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="/2018/01/foods-for-your-teeth.html"}a_to_ve[i].href="http://www.sikomos.tech"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
